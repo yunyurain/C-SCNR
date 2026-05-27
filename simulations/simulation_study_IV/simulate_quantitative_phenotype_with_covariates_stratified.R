1c217313cabffdc867a9fb78c1e80648cf2c1587
yunyurain <u3563750@connect.hku.hk>
2026-05-27 17:44:17 +0800

    Update simulate_quantitative_phenotype_with_covariates_stratified.R

--- simulations/simulation_study_IV/simulate_quantitative_phenotype_with_covariates_stratified.R
+++ simulations/simulation_study_IV/simulate_quantitative_phenotype_with_covariates_stratified.R
@@ -14,22 +14,38 @@ for (rep in 1:20) {
   ## get covariates and quantile normalize
   sample_covar = fread("UKBB_individuals/covar/covar.txt", header = T)
   covar = sample_covar[,c("SEX","AGE","PC1")]
-  
+
+  ## Scenario 1
   covar <- covar %>%
     mutate(
       AGE_Group = if_else(AGE < 55, 1, 2),
       Base_Group = paste0("S", SEX, "A", AGE_Group) 
     )
   
+  ## Scenario 2
+  ## mutate(AGE_Group = case_when(AGE < 50 ~ 1, AGE >= 50 & AGE <= 60 ~ 2,AGE > 60 ~ 3),
+  ##        Base_Group = paste0("S", SEX, "A", AGE_Group))
+  ## Scenario 3
+  ## mutate(AGE_Group = if_else(AGE < 55, 1, 2), PC1_Group = if_else(PC1 < median(PC1), 1, 2),
+  ##        Base_Group = paste0("S", SEX, "A", AGE_Group, "P", PC1_Group))
+  
   scaled_covar = scale(covar[,c("SEX","AGE","PC1")])
   scaled_covar = as.data.frame(scaled_covar)
   colnames(scaled_covar) = c("SEX","AGE","PC1")
   scaled_covar$Base_Group = covar$Base_Group
-  
+
+  ## Scenario 1
   group_effect_map <- data.frame(
     Base_Group = c("S0A1","S0A2","S1A1","S1A2"),
     cluster_multiplier = c(0.5, 2.0, 0.5, 2.0)
   )
+  
+  ## Scenario 2
+  ## Base_Group = c("S0A1","S0A2","S0A3","S1A1","S1A2","S1A3"), 
+  ## cluster_multiplier = c(0.5, 1.0, 2.0, 0.5, 1.0, 2.0)
+  ## Scenario 3
+  ## Base_Group = c("S0A1P1", "S0A1P2", "S0A2P1", "S0A2P2","S1A1P1", "S1A1P2", "S1A2P1", "S1A2P2"),
+  ## cluster_multiplier = c(0.5, 1.0, 1.0, 2.0,0.5, 1.0, 1.0, 2.0)
   
   scaled_covar <- scaled_covar %>%
     left_join(group_effect_map, by = "Base_Group")
